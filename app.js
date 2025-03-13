fdad41ed8c7ea6a93705f13e4a8c7a33acbeacc4
liming wu <limwuly@gmail.com>
2025-03-13 16:06:54 -0400

    code refactor and inject Book model

--- app.js
+++ app.js
@@ -7,42 +7,13 @@ const db = mongoose.connect("mongodb://localhost/bookApi", {
   useNewUrlParser: true,
 });
 
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-bookRouter
-  .route("/books")
-  .post((req, res) => {
-    const book = new Book(req.body);
-
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-bookRouter.route("/books/:bookId").get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(book);
-  });
-});
+const bookRouter = require("./routes/bookRouter")(Book);
 
 app.use("/api", bookRouter);
 
