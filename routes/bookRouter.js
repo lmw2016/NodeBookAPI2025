a62cd08b8e14fb73188203dcc8acd3524e66b889
liming wu <limwuly@gmail.com>
2025-04-01 15:18:09 -0400

    implement middleware and patch

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -23,26 +23,55 @@ function routes(Book) {
       });
     });
 
-  bookRouter.route("/books/:bookId").get((req, res) => {
+  bookRouter.use("/books/:bookId", (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
       if (err) {
         return res.send(err);
       }
-      return res.json(book);
-    }); //.put((req, res) => {
-    //   Book.findById(req.params.bookId, (err, book) => {
-    //     if (err) {
-    //       return res.send(err);
-    //     }
-    //     book.title = req.body.title;
-    //     book.author = req.body.author;
-    //     book.genre = req.body.genre;
-    //     book.read = req.body.read;
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+    bookRouter
+      .route("/books/:bookId")
+      .get((req, res) => {
+        res.json(req.book);
+      })
+      .put((req, res) => {
+        const { book } = req;
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.genre = req.body.genre;
+        book.read = req.body.read;
 
-    //     book.save();
-    //     return res.json(book);
-    //   });
-    // });
+        book.save((err) => {
+          if (err) {
+            return res.send(err);
+          }
+          return res.json(book);
+        });
+      })
+      .patch((req, res) => {
+        const { book } = req;
+        //eslint-disable-next-line no-underscore-dangle
+        if (req.body._id) {
+          //eslint-disable-next-line no-underscore-dangle
+          delete req.body._id;
+        }
+        Object.entries(req.body).forEach((item) => {
+          const key = item[0];
+          const value = item[1];
+          book[key] = value;
+        });
+        req.book.save((err) => {
+          if (err) {
+            return res.send(err);
+          }
+          return res.json(book);
+        });
+      });
   });
 
   return bookRouter;
